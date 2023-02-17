9fbdc478c0cf697cc55229f3b744c5b604d1d298
leeleejueun <leejueun413@gmail.com>
2023-02-17 18:04:06 +0900

    css적용

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,16 +11,17 @@ export default function NavBar() {
         <Link href="/about" className={[styles.link, router.pathname ==='/about' ? styles.active : ""].join(" ")}>About</Link>*/}
         
         {/* styles jsx  a태그를 같이 쓸수있는 방법은 있으나 권장하지 않음 */}
-        <Link legacyBehavior href="/"><a>Home</a></Link>
-        <Link legacyBehavior href="/about"><a>About</a></Link>
+        <Link legacyBehavior href="/"><a className={router.pathname === "/" ? "active" : ""}>Home</a></Link>
+        <Link legacyBehavior href="/about"><a className={router.pathname === "/about" ? "active" : ""}>About</a></Link>
         <style jsx>{`
-            nav{
-                background-color : tomato;
-            }
             a{
                 text-decoration: none;
             }
+            .active{
+                color:tomato;
+            }
         `}</style>
+        
     </nav>
     )
 }
